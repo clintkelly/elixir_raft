1b75d30d2c52468fe0b71cfe23c0d22f9dfe681a
Clint Kelly <clint.kelly@airbnb.com>
2017-05-26 10:36:42 -0700

    Create empty project.

---
+++ lib/elixir_raft.ex
@@ -0,0 +1,18 @@
+defmodule ElixirRaft do
+  @moduledoc """
+  Documentation for ElixirRaft.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirRaft.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixir_raft_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixirRaftTest do
+  use ExUnit.Case
+  doctest ElixirRaft
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
